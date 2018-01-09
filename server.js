0df0602e5c48593b88c15f6cc938a5bcf943b3ca
Victor Kazuyuki Shinya <contato@victorshinya.com.br>
2018-01-09 15:10:35 -0200

    Set up server

---
+++ server.js
@@ -0,0 +1,17 @@
+// https://github.com/expressjs/body-parser
+const bodyParser = require('body-parser')
+// https://github.com/motdotla/dotenv
+const dotenv = require('dotenv').config()
+// https://github.com/expressjs/express
+const express = require('express')
+// Set up Expressjs with Body-Parser library
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+// Set up server config
+const port = process.env.PORT || process.env.VCAP_APP_PORT || 3000
+const host = process.env.HOST || '0.0.0.0'
+// Start server
+app.listen(port, host, () => {
+    console.log(`Server is up and running at ${host}:${port}`)
+})
